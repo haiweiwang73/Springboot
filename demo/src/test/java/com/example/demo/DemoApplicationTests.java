1c5e642e59c0834f67d882b452d31820de09fbe9
HaiWei Wang <haiwei73@gmail.com>
2020-04-30 14:41:20 -0700

    Update DemoApplicationTests.java

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,7 +2,12 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//implement test case 
 
+//learn docker compose
+//lear some basic docker 
+//create a docker container with database
+//connect to the docker database for test
 @SpringBootTest
 class DemoApplicationTests {
 
